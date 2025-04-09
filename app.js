83f04a89c0da54996a18e6ebcca9e85b8ae4e0f1
KotiyaReba <knowledge.office3@ictkerala.org>
2025-04-09 15:00:26 +0530

    .env added

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require('express');
-require('./db');
+var dotenv = require('dotenv');
+
 var userRoutes = require('./routes/userRoute')
 var app = express();
 
+dotenv.config();
+require('./db');
+var port = process.env.port;
+
 app.use(express.json());
-var port = 4000;
+
 // changes
 app.use('/api',userRoutes)
 app.listen(port,(req,res)=>{
--- db.js
+++ db.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 // mongoose.connect("url").then(()=>{}).catch(()=>{})
 mongoose.
-connect("mongodb+srv://user1:SHGxhrdz7VZVWYx6@cluster0.1qg89hd.mongodb.net/nIRbATCH1?retryWrites=true&w=majority&appName=Cluster0")
+connect(process.env.mongodb_url)
 .then(()=>{
     console.log("CONNECTED TO DB")
 })
