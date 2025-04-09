662e61bbb9e3bdd715a9f012c93913d71fe603cd
KotiyaReba <knowledge.office3@ictkerala.org>
2025-04-09 13:45:52 +0530

    first commitjhss

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 
 app.use(express.json());
 var port = 4000;
-
+// changes
 app.use('/api',userRoutes)
 app.listen(port,(req,res)=>{
     console.log(`Server is up and running in ${port}`)
